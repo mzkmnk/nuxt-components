31afac55e3935a8465ecc6092e59840ea79425da
sma0729mex <sma0729mex@gmail.com>
2024-10-05 01:32:42 +0000

    add:tailwind&primevue

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex flex-col">
+    <p class="text-5xl">test</p>
+    <Button class="w-64" label="test"></Button>
+    <InputText placeholder="Overriden" class="m-2" />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura';
+
 export default defineNuxtConfig({
+  ssr:false,
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@primevue/nuxt-module'
+  ],
+  // css: [
+  //   '@/assets/css/tailwind.css'
+  // ],
+  primevue: {
+    options: {
+        theme: {
+            preset: Aura
+        }
+    }
+  },
 })
